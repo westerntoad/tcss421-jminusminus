352ff56cce5a3c3f5399fbdcc19f053975cc4d7f
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2025-01-16 16:02:52 -0800

    Add test file for new keywords

---
+++ tests/jvm/NewKeywordsTest.java
@@ -0,0 +1,33 @@
+public class NewKeywordsTest {
+    public static void main(String[] args) {
+        // Using each of the 28 new keywords in separate statements
+        continue;
+        for (int i = 0; i < 10; i++) {}
+        switch (x) { case 1: break; }
+        assert x > 0;
+        default: break;
+        synchronized (this) {}
+        do {} while (false);
+        goto label;
+        break;
+        double d = 0.0;
+        class ImplementsTest implements Runnable { public void run() {} }
+        throw new Exception();
+        byte b = 0;
+        void method() throws Exception {}
+        case 1: break;
+        enum E { A, B, C }
+        transient int x;
+        try {} catch (Exception e) {}
+        short s = 0;
+        try {} finally {}
+        final int y = 0;
+        interface I {}
+        finally {}
+        long l = 0L;
+        volatile int z;
+        const int c = 0;
+        float f = 0.0f;
+        native void method();
+    }
+}
