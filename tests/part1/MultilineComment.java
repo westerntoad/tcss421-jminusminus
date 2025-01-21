d897ed7e465bb8f1bba79e584f476d902f7295c9
westerntoad <abr.engebretson@gmail.com>
2025-01-21 13:07:16 -0800

    part1 test

---
+++ tests/part1/MultilineComment.java
@@ -0,0 +1,10 @@
+import java.lang.System;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*
+         * This is a multiline comment!
+         */
+        System.out.println("Hello, world!");
+    }
+}
