5a25a2ca3d6802c462ef9df0a25ff379a53ce4ca
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2025-01-23 13:56:06 -0800

    New literal testing

--- tests/jvm/NewLiteralsTest.java
+++ tests/jvm/NewLiteralsTest.java
@@ -2,17 +2,51 @@ public class NewLiteralsTest {
     public static void main(String[] args) {
         double i = 0.01d;
         double j = .005;
-        double k = 5.0d;
+        double k = 0xAp-5;
         double l = 4.;
         double s = 3_3__2.0;
         double x = 2_0.0_0;
         double y = 2___2.0__012_______2d;
-        System.out.println("i: " + i);
-        System.out.println("j: " + j);
-        System.out.println("k: " + k);
-        System.out.println("l: " + l);
-        System.out.println("s: " + s);
-        System.out.println("x: " + x);
+        float z = 0.e+1f;
+        float q = 0_0.e-1f;
+        float f1 = 1.23f;
+        float f2 = 4.56e+2f;
+        float f3 = .789f;
+        float f4 = 9_9.9_9f;
+        double f5 = 0x165498F.9AAp3;
+        float f10 = 0x.0p3f;
+        float f21 = 0x1.1111111p1f;
+        double hex2 = 0x1.fp3;
+        float f6 = 0x1p0f;
+        int o = 010;
+        float p = -.0f;
+        float poo = 0x0111fff;
+        double pee = 0b100110011010010;
+        double weep = 0x1a.00p11;
+        int bin = 0b01___010101__1;
+        double pap = .0_1;
+        Long yappie = 0xAA3L;
+        Long yap = 000_000_000L;
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
+        System.out.println("s = " + s);
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        System.out.println("q = " + q);
+        System.out.println("f1 = " + f1);
+        System.out.println("f2 = " + f2);
+        System.out.println("f3 = " + f3);
+        System.out.println("f4 = " + f4);
+        System.out.println("f5 = " + f5);
+        System.out.println("f6 = " + f6);
+        System.out.println("o = " + o);
+        System.out.println("p = " + p);
+        System.out.println("poo = " + poo);
+        System.out.println("pee = " + pee);
+        System.out.println("pap = " + pap);
 
     }
 }
