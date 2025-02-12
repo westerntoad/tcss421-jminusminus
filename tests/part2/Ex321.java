b37603819a489e2f4779f028ec80c409f3847ec7
westerntoad <abr.engebretson@gmail.com>
2025-02-12 11:24:56 -0800

    Add tests & add out/ to gitignore

---
+++ tests/part2/Ex321.java
@@ -0,0 +1,69 @@
+public class Ex321 {
+    public static void main(String[] args) {
+        // Decimal float
+        float float1 = 0.01f;
+        /*float float2 = .0e+1f;
+        float float3 = .1f;
+        float float4 = 123.456f;
+        float float5 = 789.0123f;
+        float float6 = 1000.f;
+        float float7 = 001120001f;
+        float float8 = 3_3__2.0f;
+        float float9 = 2_0.0_0f;
+        float float10 = 2___2.0__012_______2f;
+        float float11 = .0_1f;
+        float float12 = 1_234.567_89f;
+        float float13 = 0.000_001f;
+        float float14 = 1_2.3_4e+5f;
+        float float15 = 1_2.3_4e-5f;
+        float float16 = 1_2.3_4e1_6f;
+        float float17 = 1_2.3_4e-1_6f;
+        float float18 = 1_2.3_4E+5f;
+        float float19 = 1_2.3_4E-5f;
+        float float20 = 0008f;
+        float float21 = 01f;
+        float float22 = 01234591f;
+
+        // Hexadecimal float
+        float hexFloat1 = 0x1.0p0f;
+        float hexFloat2 = 0x1.8p1f;
+        float hexFloat3 = 0x1.fp3f;
+        float hexFloat4 = 0x1a.00p11f;
+        float hexFloat5 = 0x1.0p-1f;
+        float hexFloat6 = 0x1.0p-2f;
+        float hexFloat7 = 0xff.ffp+1f;
+        float hexFloat8 = 0x.1p1f;
+        float hexFloat16 = 0xff.1p1f;
+
+        // Decimal double
+        double double1 = 0.01d;
+        double double2 = .0e+1;
+        double double3 = .1;
+        double double4 = 123.456;
+        double double5 = 789.0123d;
+        double double6 = 1000.;
+        double double7 = 11e2f;
+        double double8 = 3_3__2.0;
+        double double9 = 2_0.0_0;
+        double double10 = 2___2.0__012_______2d;
+        double double11 = .0_1;
+        double double12 = 1_234.567_89;
+        double double13 = 10.000_001;
+        double double14 = 101.e1;
+        double double15 = 1_2.3_4e+5;
+        double double16 = 1_2.3_4e-5;
+        double double17 = 1_2.3_4e5_6;
+        double double18 = 1_2.3_4e-5_6;
+        double double19 = 1_2.3_4E+5;
+        double double20 = 1_2.3_4E-5;
+        double double21 = 001d;
+
+        // Hexadecimal double
+        double hexDouble1 = 0xAp-5;
+        double hexDouble2 = 0x165498F.9AAp3;
+        double hexDouble3 = 0x1.fp3;
+        double hexDouble4 = 0x1a.00p11;
+        double hexDouble5 = 0x1.0p0;
+        double hexDouble6 = 0x1.8p1;*/
+    }
+}
