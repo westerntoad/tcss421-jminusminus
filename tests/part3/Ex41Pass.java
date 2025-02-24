3122328963c9514027824fc1790fa340b41126dd
Jsbrenio <brenio.jeremiah.2021@gmail.com>
2025-02-24 13:50:43 -0800

    Made tests

---
+++ tests/part3/Ex41Pass.java
@@ -0,0 +1,15 @@
+import java.lang.System;
+
+public class Ex41Pass {
+    public static void main(String[] args) {
+        System.out.println("SHOULD PASS");
+    }
+}
+
+abstract class abstractClass {
+
+}
+
+class childClass extends abstractClass {
+
+}
--- tests/part3/Ex42.java
+++ tests/part3/Ex42.java
@@ -3,9 +3,8 @@ import java.lang.System;
 public class Ex42 {
     public static void main(String[] args) {
 
+        int a = 5 + x;
+        int b = a + x;
+        int c = a + b + x;
     }
-}
-
-public class Helper {
-    
 }
