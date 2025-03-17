b73a1275c58bb3077b228c2a0d4552073167fe36
CYoung5233 <cyoung17@uw.edu>
2025-03-16 23:40:46 -0700

    Assignment 5.7

--- src/jminusminus/JForStatement.java
+++ src/jminusminus/JForStatement.java
@@ -8,6 +8,8 @@ import static jminusminus.CLConstants.*;
 
 /**
  * The AST node for a for-statement.
+ *  @version 2.0
+ *  @author Corey Young
  */
 class JForStatement extends JStatement {
     // Initialization.
@@ -40,19 +42,49 @@ class JForStatement extends JStatement {
         this.body = body;
     }
 
+    //Assignment 5.7
+    //Check inits (can be multiple seperated by commas)
+    //check condition (there can be only one)
+    //check updates (can be multiple seperated by commas)
+    //check body & return
     /**
      * {@inheritDoc}
      */
     public JForStatement analyze(Context context) {
-        // TODO
+        for(JStatement initStatement : init) {
+            initStatement = (JStatement) initStatement.analyze(context);
+        }
+        condition = condition.analyze(context);
+        condition.type().mustMatchExpected(line, Type.BOOLEAN);
+        for(JStatement updateStatement : update) {
+            updateStatement = (JStatement) updateStatement.analyze(context);
+        }
+        body = (JStatement) body.analyze(context);
         return this;
     }
 
+    //generate initializers
+    //generate condition check
+    //generate body
+    //Afterwards generate updates
+    //Jump back to condition check
     /**
      * {@inheritDoc}
      */
     public void codegen(CLEmitter output) {
-        // TODO
+        String conditionCheck = output.createLabel();
+        String exitLoop = output.createLabel();
+        for (JStatement initStatement : init) {
+            initStatement.codegen(output);
+        }
+        output.addLabel(conditionCheck);
+        condition.codegen(output, exitLoop, false);
+        body.codegen(output);
+        for (JStatement updateStatement : update) {
+            updateStatement.codegen(output);
+        }
+        output.addBranchInstruction(GOTO, conditionCheck);
+        output.addLabel(exitLoop);
     }
 
     /**
